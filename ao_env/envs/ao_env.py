f858c7308b90d10acdc2e0047d3f1e1ccfcc46a1
Zoya))) <zoya.v@ya.ru>
2021-07-15 21:44:36 +0300

    fix reset

--- ao_env/envs/ao_env.py
+++ ao_env/envs/ao_env.py
@@ -42,7 +42,7 @@ class AdaptiveOptics(gym.Env):
             self.data_loaded = yaml.safe_load(stream)
 
         self.action_space = spaces.Box(-2, 2, shape=(32,))
-        self.observation_space = spaces.Box(0, 1, shape=(128, 128,3), dtype=np.float32)
+        self.observation_space = spaces.Box(0, 255, shape=(128, 128,3), dtype=np.uint8)
         self._initao()
 
     def _initao(self):
@@ -62,7 +62,8 @@ class AdaptiveOptics(gym.Env):
 
     def step(self, action):
         loopFrame(self.sim, action)
-        next_state = self.sim.sciImgs[0].copy() / (np.max(self.sim.sciImgs[0]))
+        next_state = self.sim.sciImgs[0].copy() / (np.max(self.sim.sciImgs[0])) * 255
+        next_state = next_state.astype(np.uint8)
         reward = np.sum(next_state) ** 2 / np.sum(next_state ** 2)
         x = next_state.reshape(1, 128,128)
         return np.vstack([x,x,x]).T, reward, False, {}
