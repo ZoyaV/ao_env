dc4727e4683cc03f5c7a2ce3b5ae101534c6481d
Zoya))) <zoya.v@ya.ru>
2021-07-16 17:21:03 +0300

    true reward

--- ao_env/envs/ao_env.py
+++ ao_env/envs/ao_env.py
@@ -42,7 +42,7 @@ class AdaptiveOptics(gym.Env):
             self.data_loaded = yaml.safe_load(stream)
         self.__counter = 0
         self.mem_img = []
-
+        self.expert_commands = []
         self.action_space = spaces.Box(-2, 2, shape=(32,))
         self.observation_space = spaces.Box(0, 255, shape=(128, 128,3), dtype=np.uint8)
         self._initao()
@@ -54,7 +54,8 @@ class AdaptiveOptics(gym.Env):
         self.sim.makeIMat()
 
         self.step(self.expert())
-        self.step(self.expert())
+        self.pre_expert_value = self.expert()
+        self.step(self.pre_expert_value)
 
     def expert(self):
         if self.sim.config.sim.nDM:
@@ -63,22 +64,23 @@ class AdaptiveOptics(gym.Env):
         return commands
 
     def step(self, action):
-        loopFrame(self.sim, action)
+        expert_value = self.expert()
+
+        loopFrame(self.sim,expert_value)
         img = self.sim.sciImgs[0].copy()
         next_state = ((img - np.min(img)) / (np.max(img) - np.min(img)) )* 255
         next_state = next_state.astype(np.uint8)
-
-        reward = np.sum(next_state ** 2)/np.sum(next_state) ** 2
-        reward*=10000
-
-        if reward > 0.05:
-            reward*=2
-        #reward = reward
         x = next_state.reshape(1, 128, 128)
         self.mem_img.append(x)
+        state = self.mem_img[:3]
         self.mem_img = next_state[-3:]
+
+        reward = (action - self.pre_expert_value)**2
+        reward = - reward
+
         self.__counter += 1
-        return np.vstack(self.mem_img).T, reward, False, {}
+        self.pre_expert_value = expert_value
+        return np.vstack(state).T, reward, False, {}
 
 
 
