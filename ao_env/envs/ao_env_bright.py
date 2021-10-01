4cf271f53d574d9317f416cd786238227c1bffdf
Zoya))) <zoya.v@ya.ru>
2021-10-01 12:50:11 +0300

    0.006 threshold

--- ao_env/envs/ao_env_bright.py
+++ ao_env/envs/ao_env_bright.py
@@ -77,7 +77,7 @@ class AdaptiveOpticsBright(gym.Env):
         return commands
 
     def check_done(self, reward):
-        if reward > 0.13 or reward < 0.006:
+        if reward > 0.13 or reward < 0.0055:
             return True
         else:
             return False
